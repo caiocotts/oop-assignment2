c787f40abdf194aa00f3183b8074f6e645d5df96
keag42 <keagancam@gmail.com>
2025-03-25 16:12:15 -0400

    added College

---
+++ College.cs
@@ -0,0 +1,12 @@
+namespace Assignment2;
+
+public class College {
+    // List of Students
+    // matrix List of enrollment??
+    //2d array holding something or other
+    
+    //method to return all students
+    // maybe make a option menu for choosing students?
+    
+    //method to return student count and course count
+}
