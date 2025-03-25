91b7f487c59ce6f0431f8b5b503b469777a81964
keag42 <keagancam@gmail.com>
2025-03-25 18:06:34 -0400

    added enrollStudent method

--- College.cs
+++ College.cs
@@ -2,10 +2,10 @@ namespace Assignment2;
 
 public class College
 {
+    public List<Student> students = new();
     private Dictionary<int, Course> _courses = new();
 
-    public College()
-    {
+    public College() {
     }
 
     void AddCourse(Course course) => _courses.Add(course.GetCourseId(), course);
@@ -13,6 +13,15 @@ public class College
     void AddStudentToCourse(int courseId, Student studentId) => _courses[courseId].EnrollStudent(studentId);
 
 
+    //Helper
+    public void EnrollStudent() {
+        Console.WriteLine("what is this students name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("what is this students email?");
+        string email = Console.ReadLine();
+        
+        students.Add(new Student(name, email));
+    }
     //2d array holding something or other
 
     //method to return all students
