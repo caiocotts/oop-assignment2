a4b68436e35f48cea92fb1ec278c3b7fff6338e8
keag42 <keagancam@gmail.com>
2025-03-25 17:10:09 -0400

    removed student dic & enrollment method

--- College.cs
+++ College.cs
@@ -1,15 +1,11 @@
 namespace Assignment2;
 
 public class College {
-    private Dictionary<int, Student> _students = new();
     private Dictionary<int, Course> _courses = new();
 
     public College()
     {
     }
-
-    void EnrollStudent(Student student) => _students.Add(student.GetStudentID(), student);
-
     void AddCourse(Course course) => _courses.Add(course.GetCourseId(), course);
 
     void AddStudentToCourse(int courseId, int studentId )
