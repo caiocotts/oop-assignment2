325ad61c54421e5250ddc26b7fc394e5621b1cb6
keag42 <keagancam@gmail.com>
2025-03-25 16:23:45 -0400

    added lists

--- College.cs
+++ College.cs
@@ -1,12 +1,14 @@
 namespace Assignment2;
 
 public class College {
-    // List of Students
-    // matrix List of enrollment??
+    List<Student> students = new List<Student>();
+    List<Course> courses = new List<Course>();
+    
     //2d array holding something or other
     
     //method to return all students
     // maybe make a option menu for choosing students?
     
     //method to return student count and course count
+    
 }
